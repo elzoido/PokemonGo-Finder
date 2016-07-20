80bdef0cca1dfc4fd1cb84ba6c120af46e3b9c82
Sebastian Sproesser <elzoido@elbloggo.de>
2016-07-20 18:21:04 +0200

    Nicer Icons

--- main.py
+++ main.py
@@ -809,7 +809,7 @@ def get_pokemarkers():
             'type': 'pokemon',
             'key': pokemon_key,
             'disappear_time': pokemon['disappear_time'],
-            'icon': 'static/icons/%d.png' % pokemon["id"],
+            'icon': 'static/hq-icons/%d.png' % pokemon["id"],
             'lat': pokemon["lat"],
             'lng': pokemon["lng"],
             'infobox': label
